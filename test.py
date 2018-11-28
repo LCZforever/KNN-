83e0550bd7464e5757d1b7248c48a3c3b22e3284
LCZforever <794295908@qq.com>
2018-11-29 00:21:34 +0800

    写了读取图片和预处理

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 # 相当于草稿本，随便写点什么
 # 用于测试整个程序，生成准确率和错误信息
+from multiprocessing import Pool
+
+def haha(num):
+    return num
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    re_list = pool.map(haha, [i for i in range(10)])
+    print(re_list)
